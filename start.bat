bb626bbefeaeb3a74e397e5285dce22ebf4dba37
chindrus <92249823+chindrus@users.noreply.github.com>
2021-10-21 20:59:38 +0300

    Create start.bat

---
+++ start.bat
@@ -0,0 +1,14 @@
+@ ECHO  OPRIT
+ECHO ===========================
+ECHO Pornește Lavalink
+ECHO ===========================
+porniți  cmd /k java -jar ./Lavalink.jar
+ECHO ===========================
+@ ECHO Luând o pauză de 5 secunde pentru Lavalink
+ECHO ===========================
+timeout /T 5 /nobreak
+ECHO ===========================
+@ ECHO Pornirea BOT
+ECHO ===========================
+porniți  nodul cmd /k.
+ieșire /s
